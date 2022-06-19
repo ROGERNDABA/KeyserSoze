b28e24e3962635a7af6b7aaae33e7372791173c3
rogerndaba <rogerndaba@gmail.com>
2022-06-19 23:23:39 +0200

    first attempt at openCV C++ project

---
+++ main.cpp
@@ -0,0 +1,22 @@
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+
+int main() {
+	VideoCapture cap(0);
+	if (!cap.isOpened()) return -1;
+
+	while (true) {
+		Mat frame;
+
+		cap >> frame;
+
+		imshow("Capure Window", frame);
+
+		if (waitKey(30) >= 0) break;
+	}
+
+	return 0;
+}
